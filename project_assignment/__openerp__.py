a08c9f0b6e106bff0f8cb7b231733d6319abc750
Yannick Buron <yannick.buron@gmail.com>
2014-09-25 13:40:57 +0000

    Refactor/PEP8 project_assignement

--- project_assignment/__openerp__.py
+++ project_assignment/__openerp__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Yannick Buron
-#    Copyright 2013 Yannick Buron
+#    Author: Yannick Buron. Copyright Yannick Buron
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -11,31 +10,40 @@
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-{'name': 'Project Assignment',
- 'version': '1.0',
- 'category': 'Project',
- 'depends': ['base_recursive_model',
-             'project',
-             ],
- 'author': 'Yannick Buron',
- 'license': 'AGPL-3',
- 'website': 'https://launchpad.net/',
- 'description': """
+{
+    'name': 'Project Assignment',
+    'version': '1.0',
+    'category': 'Project',
+    'author': 'Yannick Buron',
+    'license': 'AGPL-3',
+    'description': """
 Project Assignment
 =================
+
+Automatically reassign task to specified partner depending on stage
+-------------------------------------------------------------------
+    * For each stage, the partner can be specified in stage, then in project and finally task itself
+    * We use partner instead of user for more flexibility
+    * Use base inherit config
 """,
- 'demo': ['data/project_assignment_demo.xml'],
- 'data': ['security/ir.model.access.csv',
-          'project_assignment_view.xml'],
- 'test': ['tests/project_assignment_test.yml'],
- 'installable': True,
- 'application': True,
+    'website': 'https://github.com/YannickB/community-management',
+    'depends': [
+        'base_recursive_model',
+        'project',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'project_assignment_view.xml'
+    ],
+    'demo': ['data/project_assignment_demo.xml'],
+    'test': ['tests/project_assignment_test.yml'],
+    'installable': True,
 }
--- project_assignment/project_assignment.py
+++ project_assignment/project_assignment.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Yannick Buron
-#    Copyright 2013 Yannick Buron
+#    Author: Yannick Buron. Copyright Yannick Buron
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -11,30 +10,27 @@
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-import openerp.addons.decimal_precision as dp
+import logging
 
-from openerp import netsvc
-from openerp import pooler
-from openerp import SUPERUSER_ID
 from openerp.osv import fields, osv, orm
 from openerp.tools.translate import _
-from operator import itemgetter
 
-import logging
-#_logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
-class project_task_type(osv.osv):
-    ''' vote_category is meant to be inherited by any model which will define vote type
-        for linked records.
-    '''
+
+class ProjectTaskType(osv.osv):
+
+    """
+    Specify the default assigned partner for each stage, and trigger config update on each project when changed
+    """
     _inherit = 'project.task.type'
 
     _columns = {
@@ -42,12 +38,14 @@ class project_task_type(osv.osv):
     }
 
     def _boolean_update_projects(self, cr, uid, vals, context=None):
+        # Check if we should update projects
         res = False
         if 'partner_id' in vals:
             res = True
         return res
 
     def _update_assigned_partner(self, cr, uid, ids, vals, context=None):
+        # Update config in project
         project_obj = self.pool.get('project.project')
         if self._boolean_update_projects(cr, uid, vals, context=context):
             project_ids = {}
@@ -57,24 +55,25 @@ class project_task_type(osv.osv):
                     project_ids[project.id] = project.id
             project_obj._update_stored_config(cr, uid, list(project_ids), context=context)
 
-
     def create(self, cr, uid, vals, context=None):
-        res = super(project_task_type, self).create(cr, uid, vals, context=context)
+        # Trigger update config on create
+        res = super(ProjectTaskType, self).create(cr, uid, vals, context=context)
         self._update_assigned_partner(cr, uid, [res], vals, context=context)
         return res
 
-
     def write(self, cr, uid, ids, vals, context=None):
-        res = super(project_task_type, self).write(cr, uid, ids, vals, context=context)
+        # Trigger update config on write
+        res = super(ProjectTaskType, self).write(cr, uid, ids, vals, context=context)
         self._update_assigned_partner(cr, uid, ids, vals, context=context)
         return res
 
 
+class ProjectAssignedPartnerModel(osv.AbstractModel):
 
-class project_assigned_partner_model(osv.AbstractModel):
-    ''' vote_category is meant to be inherited by any model which will define vote type
-        for linked records.
-    '''
+    """
+    Abstract class used by project and task to create config lines. Inherit base.config.inherit.model.
+    """
+
     _name = 'project.assigned.partner.model'
     _inherit = ['base.config.inherit.model']
 
@@ -83,68 +82,78 @@ class project_assigned_partner_model(osv.AbstractModel):
     _base_config_inherit_o2m = 'assigned_partner_config_ids'
 
     _columns = {
-        'assigned_partner_config_ids': fields.one2many('project.assigned.partner.config', 'res_id',
-            domain=lambda self: [('model', '=', self._name),('stored','=',False)],
+        'assigned_partner_config_ids': fields.one2many(
+            'project.assigned.partner.config', 'res_id',
+            domain=lambda self: [('model', '=', self._name), ('stored', '=', False)],
             auto_join=True,
-            string='Assigned Partner configuration'),
-        'assigned_partner_config_result_ids': fields.one2many('project.assigned.partner.config', 'res_id',
-            domain=lambda self: [('model', '=', self._name),('stored','=',True)],
+            string='Assigned Partner configuration'
+        ),
+        'assigned_partner_config_result_ids': fields.one2many(
+            'project.assigned.partner.config', 'res_id',
+            domain=lambda self: [('model', '=', self._name), ('stored', '=', True)],
             auto_join=True,
-            string='Assigned Partner', readonly=True),
+            string='Assigned Partner', readonly=True
+        ),
     }
 
-
     def _prepare_config(self, cr, uid, id, record, vals={}, context=None):
+        # Specify the fields contained in the configuration
         res = {
             'model': self._name,
             'res_id': id,
             'stage_id': 'stage_id' in record and record.stage_id.id or False,
             'partner_id': 'partner_id' in record and record.partner_id.id or False,
-            'sequence': 'sequence' in record and record.sequence or 'stage_id' in record and record.stage_id.sequence or False,
+            'sequence': 'sequence' in record
+                        and record.sequence or 'stage_id' in record and record.stage_id.sequence or False,
             'stored': True
         }
 
-        res.update(super(project_assigned_partner_model, self)._prepare_config(cr, uid, id, record, vals=vals, context=context))
-        #_logger.info('res %s', res)
+        res.update(super(ProjectAssignedPartnerModel, self)._prepare_config(
+            cr, uid, id, record, vals=vals, context=context
+        ))
         return res
 
 
+class ProjectProject(osv.osv):
 
-
-class project_project(osv.osv):
-    ''' vote_category is meant to be inherited by any model which will define vote type
-        for linked records.
-    '''
+    """
+    Add assignment fields in project.project
+    """
 
     _name = 'project.project'
     _inherit = ['project.project', 'project.assigned.partner.model']
 
-
     def _get_external_config(self, cr, uid, record, context=None):
+        # Get configuration from stages
         res = {}
         for type in record.type_ids:
             if type.partner_id:
-                res[type.id] =  self._prepare_config(cr, uid, record.id, type, vals={'stage_id': type.id}, context=context)
-        ##_logger.info('res %s', res)
+                res[type.id] = self._prepare_config(
+                    cr, uid, record.id, type,
+                    vals={'stage_id': type.id}, context=context
+                )
         return res
 
     def _get_child_ids(self, cr, uid, ids, context=None):
+        # Get project childs
         analytic_ids = {}
         for project in self.browse(cr, uid, ids, context=context):
             analytic_ids[project.analytic_account_id.id] = project.analytic_account_id.id
-        return self.search(cr, uid, [('parent_id','in', list(analytic_ids))], context=context)
+        return self.search(cr, uid, [('parent_id', 'in', list(analytic_ids))], context=context)
 
     def _update_stored_config_external_children(self, cr, uid, ids, context=None):
+        # Trigger update config in tasks linked to this project
         task_obj = self.pool.get('project.task')
         task_ids = task_obj.search(cr, uid, [('project_id', 'in', ids)], context=context)
         task_obj._update_stored_config(cr, uid, task_ids, context=context)
         return True
 
 
-class project_task(osv.osv):
-    ''' vote_category is meant to be inherited by any model which will define vote type
-        for linked records.
-    '''
+class ProjectTask(osv.osv):
+
+    """
+    Add assignment fields in project.task and recompute assigned partner when we change stage
+    """
 
     _name = 'project.task'
     _inherit = ['project.task', 'project.assigned.partner.model']
@@ -155,6 +164,7 @@ class project_task(osv.osv):
     }
 
     def _get_external_config(self, cr, uid, record, context=None):
+        # Get configuration from project
         res = {}
         if record.project_id:
             for config in record.project_id.assigned_partner_config_result_ids:
@@ -162,10 +172,15 @@ class project_task(osv.osv):
         return res
 
     def _update_assigned_partner(self, cr, uid, ids, vals, context=None):
+        # Update partner either from argument or from configuration
         if 'user_id' in vals:
-            vals['assigned_partner_id'] = self.pool.get('res.users').browse(cr, uid, vals['user_id'], context=context).partner_id.id
+            vals['assigned_partner_id'] = self.pool.get('res.users').browse(
+                cr, uid, vals['user_id'], context=context
+            ).partner_id.id
         if 'reviewer_id' in vals:
-            vals['reviewer_partner_id'] = self.pool.get('res.users').browse(cr, uid, vals['reviewer_id'], context=context).partner_id.id
+            vals['reviewer_partner_id'] = self.pool.get('res.users').browse(
+                cr, uid, vals['reviewer_id'], context=context
+            ).partner_id.id
 
         if 'stage_id' in vals and not 'assigned_partner_id' in vals:
             for task in self.browse(cr, uid, ids, context=context):
@@ -188,26 +203,25 @@ class project_task(osv.osv):
 
         return vals
 
-
-
-
-
     def create(self, cr, uid, vals, context=None):
-        #_logger.info('task vals create %s', vals)
-        res = super(project_task, self).create(cr, uid, vals, context=context)
+        # Force write function to be called on create
+        res = super(ProjectTask, self).create(cr, uid, vals, context=context)
         self.write(cr, uid, [res], vals, context=context)
         return res
 
-
     def write(self, cr, uid, ids, vals, context=None):
-        #_logger.info('task vals write %s', vals)
+        # Trigger the function to update the assigned partner on write
         vals = self._update_assigned_partner(cr, uid, ids, vals, context=context)
-        ##_logger.info('vals %s', vals)
-        res = super(project_task, self).write(cr, uid, ids, vals, context=context)
+        res = super(ProjectTask, self).write(cr, uid, ids, vals, context=context)
         return res
 
 
-class project_assigned_partner_config(osv.osv):
+class ProjectAssignedPartnerConfig(osv.osv):
+
+    """
+    Extend the configuration line with field specific to partner assignment
+    """
+
     _name = 'project.assigned.partner.config'
     _inherit = 'base.config.inherit.line'
 
@@ -215,4 +229,3 @@ class project_assigned_partner_config(osv.osv):
         'stage_id': fields.many2one('project.task.type', 'Stage', required=True),
         'partner_id': fields.many2one('res.partner', 'Assigned Partner'),
     }
-
